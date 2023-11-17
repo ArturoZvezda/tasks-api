a13e9af2e8ba1a54b7a22d1b2d564bad84209d84
ArturoZvezda <iskra.zvezda@gmail.com>
2023-11-17 04:32:36 -0600

    Adding register and login routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,12 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->group('api', ['namespace' => 'App\Controllers'], function ($routes) {
+    $routes->post('tasks', 'TaskController::createTask', ['filter' => 'auth']);
+});
+
+$routes->group('api', ['namespace' => 'App\Controllers'], function ($routes) {
+    $routes->post('register', 'AuthController::register');
+    $routes->post('login', 'AuthController::login');
+});
